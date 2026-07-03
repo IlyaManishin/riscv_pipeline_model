c9080db50a53f308c045e4390662afbe898deae3
IlyaManishin <manishin06@gmail.com>
2026-07-03 17:46:50 +0700

    add pc

---
+++ src/risc_v/modules/pc.py
@@ -0,0 +1,30 @@
+from sim_base.mem.register import Register
+
+import riscv_config as conf 
+
+class PC:
+    def __init__(self, rst_reg: Register, width: int = conf.XLEN, pc_start_addr: int = conf.PC_START_ADDR):
+        self._reg = Register(init_value=pc_start_addr)
+        
+        self.rst_reg = rst_reg
+        self._pc_start_addr = pc_start_addr
+
+    @property
+    def reg(self) -> Register:
+        return self._reg
+
+    def read(self) -> int:
+        return self._reg.read()
+
+    def set_pc(self, br_taken: bool, pc_br: int) -> None:
+        """always_ff logic"""
+        rst = self.rst_reg.read()
+        if rst:
+            next_pc = self._pc_start_addr
+        elif br_taken:
+            next_pc = pc_br
+        else:
+            next_pc = self._reg.read() + 4
+
+        self._reg.set(next_pc & conf.NUM_MASK)
+        
