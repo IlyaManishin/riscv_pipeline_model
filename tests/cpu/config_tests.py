c54392cfc9c3725fbe9a6ed9d89416f874b6b009
IlyaManishin <manishin06@gmail.com>
2026-07-08 15:52:49 +0700

    add all tests from riscv project

--- tests/cpu/config_tests.py
+++ tests/cpu/config_tests.py
@@ -1,14 +1,15 @@
 from enum import Enum
-
+from pathlib import Path
 
 TIMEOUT_ITERATIONS = 50000
 RF_DBG_NUM = 31
 
-TEST_DIR = "C:\\Users\\Lecoo\\Documents\\rv-nsu\\prg\\uBench\\hex"
+TEST_DIR = Path(__file__).resolve().parent / "uBench" / "hex"
 
 
 TRACE_ENABLE = True
 TRACE_DIR = "trace"
+
 
 class Test_Result(Enum):
     TEST_RUN = 0
