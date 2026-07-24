646947c68ef1fe235ae9b2e579517107f4eb0780
IlyaManishin <manishin06@gmail.com>
2026-07-24 18:11:55 +0700

    remove choose without real choose (useless WB_sel_t.SHIFTER_OUT flag)

--- src/risc_v/pipeline/stages/writeback.py
+++ src/risc_v/pipeline/stages/writeback.py
@@ -30,8 +30,6 @@ class WriteBack:
                 self.rf_wd3 = self.pc4
             case conf.WB_sel_t.ALU_OUT:
                 self.rf_wd3 = self.buff_mem_wb.alu_out.read()
-            case conf.WB_sel_t.SHIFTER_OUT:
-                self.rf_wd3 = self.buff_mem_wb.alu_out.read()
             case conf.WB_sel_t.DMEM_OUT:
                 self.rf_wd3 = self.buff_mem_wb.dmem_data.read()
             case _:
