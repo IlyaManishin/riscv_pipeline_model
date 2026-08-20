7551c6bb6806d0c79d16646f8cb3cf5f80709dd2
IlyaManishin <manishin06@gmail.com>
2026-08-20 16:40:51 +0700

    inverse TEST_FINISH

--- benchmarks/build.py
+++ benchmarks/build.py
@@ -25,7 +25,7 @@ from tqdm import tqdm
 
 import build_config as cfg
 import build_paths as bpaths
-from benchmarks.collect_utils import collect_tests
+from collect_utils import collect_tests
 from compiler import compile_test
 
 
--- benchmarks/compiler.py
+++ benchmarks/compiler.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 import build_config as cfg
 from riscv_linker import riscv_compiler
-from benchmarks.collect_utils import TestCase
+from collect_utils import TestCase
 
 
 @dataclass
