2b3958cfabb1242f47c002e9b2c19e72a5776a07
IlyaManishin <manishin06@gmail.com>
2026-09-17 22:53:59 +0700

    remove benchmarks

--- benchmarks/build_paths.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent
-
-# sources and build paths  
-BENCHES_DIR = ROOT_DIR / "sources"
-BUILD_DIR = ROOT_DIR / "build"
-
-TEST_LIST_NAME = "benches.lst"
